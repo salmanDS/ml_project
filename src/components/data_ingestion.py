2bb988459c47a91378d966a5bfab1f6655e93b2f
Salman <ksmdsalman2@gmail.com>
2024-03-13 16:23:19 +0530

    added data transformation and expoted pkl file for same

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,14 @@
 import os
 import sys
 import pandas as pd
+sys.path.append("")
+#from src.logger import logging
 from src.logger import logging
 from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -41,6 +45,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
